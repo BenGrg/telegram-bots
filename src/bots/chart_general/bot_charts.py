9b01d770f737420732c244edc41a02a7cb42e95a
sharkgua <benoit.george@ubirch.com>
2020-10-11 18:17:51 +0200

    fewgrf

--- src/bots/chart_general/bot_charts.py
+++ src/bots/chart_general/bot_charts.py
@@ -192,11 +192,12 @@ def refresh_chart(update: Update, context: CallbackContext):
     t_from = t_to - (k_days * 3600 * 24) - (k_hours * 3600)
 
     chat_id = update.callback_query.message.chat_id
+    message_id = update.callback_query.message.message_id
     pprint.pprint(chat_id)
 
     (message, path, reply_markup_chart) = general_end_functions.send_candlestick_pyplot(token, charts_path, k_days, k_hours, t_from, t_to)
     context.bot.send_photo(chat_id=chat_id, photo=open(path, 'rb'), caption=message, parse_mode="html", reply_markup=reply_markup_chart)
-
+    context.bot.delete_message(chat_id=chat_id, message_id=message_id)
 
 def main():
     updater = Updater(TELEGRAM_KEY, use_context=True)
