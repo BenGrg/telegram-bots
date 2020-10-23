0834dfbea93a08fb767f404f72292a5466792981
sharkgua <benoit.george@ubirch.com>
2020-10-23 15:47:38 +0200

    added how to swap

--- src/bots/boo_bank/bot_boo.py
+++ src/bots/boo_bank/bot_boo.py
@@ -291,15 +291,14 @@ def do_convert(update: Update, context: CallbackContext):
         res_req = general_end_functions.convert_to_usd_raw(1, ticker_req, graphql_client_uni, graphql_client_eth)
         if ticker_to == 'lambo':
             res = amount * (res_req / float(lambo_price_usd))
-            res_req_str = util.number_to_beautiful(round(res_req)) if round(res_req) > 10 else util.float_to_str(res_req)[0:10]
+            res_req_usd_str = util.number_to_beautiful(round(res_req)) if round(res_req) > 10 else util.float_to_str(res_req)
             res_str = util.number_to_beautiful(round(res)) if round(res) > 10 else util.float_to_str(res)[0:10]
-            message = str(amount) + " " + ticker_req + " = " + res_req_str + " USD or roughly " + res_str + " lamborghini huracan"
+            message = str(amount) + " " + ticker_req + " = " + res_req_usd_str + " USD or roughly " + res_str + " lamborghini huracan"
         else:
-            res_to_convert = general_end_functions.convert_to_usd_raw(1, ticker_to, graphql_client_uni, graphql_client_eth)
-            res = amount * (res_req / res_to_convert)
-            res_req_str = util.number_to_beautiful(round(res_req)) if round(res_req) > 10 else util.float_to_str(res_req)[0:10]
+            res = amount * (res_req / res_req)
+            res_req_usd_str = util.number_to_beautiful(round(res_req)) if round(res_req) > 10 else util.float_to_str(res_req)
             res_str = util.number_to_beautiful(round(res)) if round(res) > 10 else util.float_to_str(res)[0:10]
-            message = str(amount) + " " + ticker_req + " = " + res_req_str + " USD or " + res_str + " " + ticker_to
+            message = str(amount) + " " + ticker_req + " = " + res_req_usd_str + " USD or " + res_str + " " + ticker_to
         context.bot.send_message(chat_id=chat_id, text=message, disable_web_page_preview=True, parse_mode='html')
     else:
         context.bot.send_message(chat_id=chat_id, text="Wrong format. Please use /convert AMOUNT CURRENCY", disable_web_page_preview=True, parse_mode='html')
