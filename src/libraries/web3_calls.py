1c8632e1fc33c44946f4086ed7c76641b7b941c0
Ben <rotted_ben@protonmail.com>
2020-11-06 23:36:17 +0100

    added balance

--- src/libraries/web3_calls.py
+++ src/libraries/web3_calls.py
@@ -21,6 +21,6 @@ def get_balance_token_wallet_raw(w3, wallet, token):
     token_checksum = Web3.toChecksumAddress(token)
     wallet_checksum = Web3.toChecksumAddress(wallet)
     contract = w3.eth.contract(address=token_checksum, abi=get_abi_erc20())
-    decimals = get_decimals_contract(wallet_checksum)
-    res = contract.functions.balanceOf(wallet).call() / 10 ** decimals
+    decimals = get_decimals_contract(contract)
+    res = contract.functions.balanceOf(wallet_checksum).call() / 10 ** decimals
     return res
