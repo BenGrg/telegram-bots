d7f10ce84f86467bd1671c7faca0caacf1c4c371
ben <rotted_ben@protonmail.com>
2020-11-10 18:33:32 +0100

    Adding rcp to chonk

--- src/bots/chonk/bot_chonk.py
+++ src/bots/chonk/bot_chonk.py
@@ -146,8 +146,7 @@ def get_latest_actions(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
     query_received = update.message.text.split(' ')
     if len(query_received) == 1:
-        token_ticker = "BOOB"
-        latest_actions_pretty = general_end_functions.get_last_actions_token_in_eth_pair(token_ticker, uni_wrapper, graphql_client_uni)
+        latest_actions_pretty = general_end_functions.get_last_actions_token_in_eth_pair(name, uni_wrapper, graphql_client_uni)
         context.bot.send_message(chat_id=chat_id, text=latest_actions_pretty, disable_web_page_preview=True, parse_mode='html')
     elif len(query_received) == 2:
         token_ticker = query_received[1]
