8c6a0b4700285e4a453e258b242ec620c2471739
ben <rotted_ben@protonmail.com>
2020-11-15 21:42:24 +0100

    added ocr

--- src/libraries/common_values.py
+++ src/libraries/common_values.py
@@ -1,3 +1,3 @@
 lambo_price_usd = 220000
 
-test_error_token = "Looks like you need to either: increase slippage (see /howtoslippage) to at least 4% and/or remove the decimals from the amount of $BOOB you're trying to buy"
+test_error_token = "Looks like you need to either: increase slippage and/or remove some decimals from the amount of token you're trying to buy"
